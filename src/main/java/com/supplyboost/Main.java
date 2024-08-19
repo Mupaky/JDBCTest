5ffa5b4bca38e1b6b616ec692fe8cdad0bc8633a
Mupaky <georgi.jelev94@gmail.com>
2024-08-19 23:52:02 +0300

    repair error and crete a config file that is not uploaded

--- src/main/java/com/supplyboost/Main.java
+++ src/main/java/com/supplyboost/Main.java
@@ -1,38 +1,53 @@
 package com.supplyboost;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.*;
+import java.util.Properties;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
-    private static final String url = "jdbc:sqlserver://127.0.0.1:1433;databaseName=tasknotes";
-    private static final String name = "sa";
-    private static final String password = "Peralnq1";
+
     public static void main(String[] args) {
+        Properties props = new Properties();
+        try (InputStream input = Main.class.getClassLoader().getResourceAsStream("config.properties")) {
+            if (input == null) {
+                System.out.println("Sorry, unable to find config.properties");
+                return;
+            }// Load the properties from the file
+            props.load(input);
 
-        try (Connection connection = DriverManager.getConnection(url, name, password);
-            Statement statement = connection.createStatement();
-             ){
+            Class.forName(props.getProperty("db.driver"));
 
-            try(ResultSet resultSet = statement.executeQuery("SELECT * FROM task")){
-                while (resultSet.next()) {
-                    int taskID = resultSet.getInt("id");
-                    boolean completed = resultSet.getBoolean("completed");
-                    String description = resultSet.getString("description");
-                    String title = resultSet.getString("title");
+            try (Connection connection = DriverManager.getConnection(props.getProperty("db.url"),
+                    props.getProperty("db.username"), props.getProperty("db.password"));
+                 Statement statement = connection.createStatement();
+            ) {
 
-                    System.out.println("ID: " + taskID +
-                            ", Completed: " + completed +
-                            ", Description: " + description +
-                            ", Title: " + title);
+                try (ResultSet resultSet = statement.executeQuery("SELECT * FROM task")) {
+                    while (resultSet.next()) {
+                        int taskID = resultSet.getInt("id");
+                        boolean completed = resultSet.getBoolean("completed");
+                        String description = resultSet.getString("description");
+                        String title = resultSet.getString("title");
+
+                        System.out.println("ID: " + taskID +
+                                ", Completed: " + completed +
+                                ", Description: " + description +
+                                ", Title: " + title);
+                    }
+
+                } catch (SQLException e) {
+
                 }
 
-            }catch (SQLException e){
-
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
             }
 
-        } catch (SQLException e) {
+        } catch (IOException | ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
     }
